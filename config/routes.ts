c388ca4311ef32f8c506c4a42d23bc3d5029adb0
bobochang <m13726750698_2@163.com>
2023-05-15 15:30:10 +0800

    feat:🐰添加角色管理模块

--- config/routes.ts
+++ config/routes.ts
@@ -4,9 +4,31 @@
     layout: false,
     routes: [{ name: '登录', path: '/user/login', component: './User/Login' }],
   },
-  { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
-  { path: '/userInfo', name: '用户管理', icon: 'user', component: './UserInfo' },
   {
+    path: '/welcome',
+    name: '欢迎',
+    icon: 'https://bobocahng-1309945187.cos.ap-guangzhou.myqcloud.com/20230513/马卡龙.png',
+    component: './Welcome',
+  },
+  {
+    path: '/userInfo',
+    name: '用户管理',
+    icon: 'https://bobocahng-1309945187.cos.ap-guangzhou.myqcloud.com/20230512/饼干.png',
+    component: './UserInfo',
+  },
+  {
+    path: '/departmentInfo',
+    name: '部门管理',
+    icon: 'https://bobocahng-1309945187.cos.ap-guangzhou.myqcloud.com/20230512/热狗.png',
+    component: './DepartmentInfo',
+  },
+  {
+    path: '/roleInfo',
+    name: '角色管理',
+    icon: 'https://bobocahng-1309945187.cos.ap-guangzhou.myqcloud.com/20230512/甜甜圈.png',
+    component: './RoleInfo',
+  },
+  /*{
     path: '/admin',
     name: '管理页',
     icon: 'crown',
@@ -15,8 +37,7 @@
       { path: '/admin', redirect: '/admin/sub-page' },
       { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
     ],
-  },
-  { name: '查询表格', icon: 'table', path: '/list', component: './DepartmentInfo' },
+  },*/
   { path: '/', redirect: '/welcome' },
   { path: '*', layout: false, component: './404' },
 ];
---
+++ src/pages/RoleInfo/index.tsx
@@ -0,0 +1,226 @@
+import type { ProColumns } from '@ant-design/pro-components';
+import { ActionType, PageContainer, ProTable } from '@ant-design/pro-components';
+import { Button, Drawer, message } from 'antd';
+import { SortOrder } from 'antd/lib/table/interface';
+import React, { useRef, useState } from 'react';
+import UpdateModal from '@/pages/RoleInfo/components/UpdateModal';
+import { PlusOutlined } from '@ant-design/icons';
+import CreateModal from '@/pages/RoleInfo/components/CreateModal';
+import {
+  addRoleUsingPOST,
+  deleteRoleUsingPOST,
+  listRoleByPageUsingPOST,
+  updateRoleUsingPOST1,
+} from '@/services/YouAnSystem-backend/roleController';
+
+const TableList: React.FC = () => {
+  const [createModalVisible, handleModalVisible] = useState<boolean>(false);
+  const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
+  const [currentRow, setCurrentRow] = useState<API.DepartmentVO>();
+  const actionRef = useRef<ActionType>();
+  const [showDetail, setShowDetail] = useState<boolean>(false);
+
+  const handleAdd = async (fields: API.Role) => {
+    const hide = message.loading('正在添加');
+    try {
+      await addRoleUsingPOST({
+        ...fields,
+      });
+      hide();
+      message.success('创建成功');
+      handleModalVisible(false);
+      return true;
+    } catch (error: any) {
+      hide();
+      message.error('创建失败，' + error.message);
+      return false;
+    }
+  };
+  const handleUpdate = async (fields: API.Role) => {
+    if (!currentRow) {
+      return;
+    }
+    const hide = message.loading('修改中');
+    try {
+      await updateRoleUsingPOST1({
+        id: currentRow.id,
+        ...fields,
+      });
+      hide();
+      message.success('操作成功');
+      return true;
+    } catch (error: any) {
+      hide();
+      message.error('操作失败，' + error.message);
+      return false;
+    }
+  };
+
+  const handleRemove = async (record: API.Role) => {
+    const hide = message.loading('正在删除');
+    if (!record) return true;
+    try {
+      await deleteRoleUsingPOST({
+        id: record.id,
+      });
+      hide();
+      message.success('删除成功');
+      actionRef.current?.reload();
+      return true;
+    } catch (error: any) {
+      hide();
+      message.error('删除失败，' + error.message);
+      return false;
+    }
+  };
+
+  const columns: ProColumns<API.RuleListItem>[] = [
+    {
+      title: '角色编号',
+      dataIndex: 'id',
+      align: 'center',
+      hideInForm: true,
+    },
+    {
+      title: '角色名称',
+      dataIndex: 'roleName',
+      align: 'center',
+    },
+    {
+      title: '备注',
+      dataIndex: 'remark',
+      align: 'center',
+    },
+    {
+      title: '角色状态',
+      dataIndex: 'status',
+      align: 'center',
+      valueType: 'select',
+      valueEnum: {
+        1: {
+          text: '未开启',
+          status: 'Error',
+        },
+        0: {
+          text: '已开启',
+          status: 'Success',
+        },
+      },
+    },
+    {
+      title: '操作',
+      width: 60,
+      key: 'option',
+      valueType: 'option',
+      align: 'center',
+      render: (_, record) => [
+        <Button
+          key="config1"
+          onClick={() => {
+            handleUpdateModalVisible(true);
+            setCurrentRow(record);
+          }}
+        >
+          编辑
+        </Button>,
+        <Button
+          key="config3"
+          type="default"
+          danger
+          onClick={() => {
+            handleRemove(record);
+          }}
+        >
+          删除
+        </Button>,
+      ],
+    },
+  ];
+
+  return (
+    <PageContainer>
+      <ProTable<API.RuleListItem, API.PageParams>
+        rowKey="key"
+        actionRef={actionRef}
+        pagination={{
+          showQuickJumper: true,
+        }}
+        columns={columns}
+        search={false}
+        dateFormatter="string"
+        toolBarRender={() => [
+          <Button
+            type="primary"
+            key="primary"
+            onClick={() => {
+              handleModalVisible(true);
+            }}
+          >
+            <PlusOutlined /> 添加部门
+          </Button>,
+        ]}
+        request={async (
+          params,
+          sort: Record<string, SortOrder>,
+          filter: Record<string, React.ReactText[] | null>,
+        ) => {
+          const res: any = await listRoleByPageUsingPOST({
+            ...params,
+          });
+          if (res.data) {
+            return {
+              data: res.data.records || [],
+              success: true,
+              total: res.data.total || 0,
+            };
+          } else {
+            return {
+              data: res.data.message || [],
+              success: false,
+              total: 0,
+            };
+          }
+        }}
+      />
+      <CreateModal
+        columns={columns}
+        onCancel={() => {
+          handleModalVisible(false);
+        }}
+        onSubmit={async (values) => {
+          const success = await handleAdd(values);
+          if (success) {
+            handleModalVisible(false);
+            setCurrentRow(undefined);
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+          }
+        }}
+        visible={createModalVisible}
+      />
+      <UpdateModal
+        columns={columns}
+        onSubmit={async (value) => {
+          const success = await handleUpdate(value);
+          if (success) {
+            handleUpdateModalVisible(false);
+            setCurrentRow(undefined);
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+          }
+        }}
+        onCancel={() => {
+          handleUpdateModalVisible(false);
+          if (!showDetail) {
+            setCurrentRow(undefined);
+          }
+        }}
+        visible={updateModalVisible}
+        values={currentRow || {}}
+      />
+    </PageContainer>
+  );
+};
+export default TableList;
